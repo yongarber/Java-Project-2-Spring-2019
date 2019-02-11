8dbc1636211e40917e70b99197f35ebe33068b07
Yon Garber <garberyon@gmail.com>
2019-02-11 14:32:36 -0500

    Checking Atom on Joseph

--- MessageParser.java
+++ MessageParser.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.io.File;
+import java.util.ArrayList;
 
 public class MessageParser{
   /**
@@ -7,11 +8,18 @@ public class MessageParser{
    * @param  filename the name of a comma-separated value (.csv) file in the local directory
    * @return          An array of parsed Message objects
    */
-   String filename = "full-corpus" // I dont know if this is needed
-
+   public static void main(String[] args) throws Exception{
+   String filename = "full-corpus"; // I dont know if this is needed
+   System.out.println(parseMessages(filename));
+}
   public static Message[] parseMessages(String filename) throws Exception {
     Message[] messages = null;
-
+    List<List<String>> records = new ArrayList<>();
+    try (Scanner scanner = new Scanner(new File(filename));) {
+        while (scanner.hasNextLine()) {
+            records.add(scanner.nextLine());
+        }
+    }
     /*
     Insert your code to read the file here. All of the classes that you may need are imported already, though you may take other approaches.
      */
