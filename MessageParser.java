35716e5bf05769fb5ae9b91b060eeb65c6c69092
Yon Garber <garberyon@gmail.com>
2019-02-11 15:09:54 -0500

    We read succesfully but has problem converting string to message

--- MessageParser.java
+++ MessageParser.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MessageParser{
   /**
@@ -9,16 +10,18 @@ public class MessageParser{
    * @return          An array of parsed Message objects
    */
    public static void main(String[] args) throws Exception{
-   String filename = "full-corpus"; // I dont know if this is needed
+   String filename = "full-corpus.csv"; // I dont know if this is needed
    System.out.println(parseMessages(filename));
 }
   public static Message[] parseMessages(String filename) throws Exception {
-    Message[] messages = null;
-    List<List<String>> records = new ArrayList<>();
-    try (Scanner scanner = new Scanner(new File(filename));) {
-        while (scanner.hasNextLine()) {
-            records.add(scanner.nextLine());
-        }
+    Message[] messages = new Message[5114];
+    Scanner reader = new Scanner(new File(filename));
+    while (reader.hasNext()){
+        int i =0;
+        String str = reader.nextLine();
+        System.out.println(str);
+        messages[i]=(reader.nextLine());
+        i+=1;
     }
     /*
     Insert your code to read the file here. All of the classes that you may need are imported already, though you may take other approaches.
